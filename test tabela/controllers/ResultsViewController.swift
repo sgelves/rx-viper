1ef04d6837a16fb7db864606cb387f6fbb7c51ff
Sergio Gelves <alexander.gelvez15@gmail.com>
2019-05-11 20:50:51 -0400

    initial tableview setup

---
+++ test tabela/controllers/ResultsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ResultsViewController.swift
+//  test tabela
+//
+//  Created by user155532 on 5/11/19.
+//  Copyright © 2019 sgelves. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let resultData = ["Resultado um", "Dois", "Informacao aditional do novo resultado que e muito mais longa do que o normal"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resultData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "resultCell", for: indexPath) as! ResultCellTableViewCell
+        cell.title.text = resultData[indexPath.row]
+        print(indexPath.row)
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+
+}
---
+++ test tabela/controllers/components/ResultCellTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ResultCellTableViewCell.swift
+//  test tabela
+//
+//  Created by user155532 on 5/11/19.
+//  Copyright © 2019 sgelves. All rights reserved.
+//
+
+import UIKit
+
+class ResultCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
