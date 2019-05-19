ad3e18033f7f0399f3f3b1e79e8bfa0f92ab2e99
Sergio Gelves <alexander.gelvez15@gmail.com>
2019-05-19 15:06:22 -0400

    Service use current device location

--- test tabela/controllers/ResultsViewController.swift
+++ test tabela/controllers/ResultsViewController.swift
@@ -8,20 +8,22 @@
 
 import UIKit
 import Kingfisher
+import CoreLocation
 
-class ResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, LocaisData {
-
+class ResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, CLLocationManagerDelegate, LocaisModel {
+    
     @IBOutlet weak var tableView: UITableView!
 
     var resultData: [Local] = []
     var currentPage: Int = 0
-    var isLoading: Bool = false
+    var isFetchingLoais: Bool = false
+    
+    private var coordinate: CLLocationCoordinate2D?
+    var locationManager: CLLocationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.getNextPage {
-            self.tableView.reloadData()
-        }
+        self.requestLocation()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -34,21 +36,95 @@ class ResultsViewController: UIViewController, UITableViewDataSource, UITableVie
         cell.title.text = result.placeName
         
         if (result.placePhoto != nil) {
-            cell.thumbnail.kf.setImage(with: URL(string: result.placePhoto!))
+            cell.thumbnail.kf.setImage(with: URL(string: result.placePhoto!), options: [.diskCacheExpiration(.days(9))])
         }
         
         return cell
     }
     
+    /*
+        Infinite scroll functionality
+    */
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let offset = scrollView.contentOffset.y
         let contentHeight = scrollView.contentSize.height
         
-        if offset > contentHeight - scrollView.frame.height * 2 {
-            self.getNextPage {
+        if (offset > contentHeight - scrollView.frame.height * 2) && self.coordinate != nil {
+            self.getNextPage(latitude: self.coordinate!.latitude, longitude: self.coordinate!.longitude,
+                             searchString: "") {
                 self.tableView.reloadData()
             }
         }
+        
+    }
+    
+    /*
+        Requesting location functionality
+    */
+    func requestLocation () {
+        if CLLocationManager.locationServicesEnabled() == true {
+            
+            switch CLLocationManager.authorizationStatus() {
+            case .notDetermined:
+                // Request when-in-use authorization initially
+                locationManager.requestWhenInUseAuthorization()
+                break
+                
+            case .restricted, .denied:
+                // Disable location features, in this case ask for permissions again
+                locationManager.requestWhenInUseAuthorization()
+                break
+                
+            case .authorizedWhenInUse, .authorizedAlways:
+                // Enable basic location features
+                locationManager.desiredAccuracy = kCLLocationAccuracyBest
+                locationManager.delegate = self
+                locationManager.startUpdatingLocation()
+                break
+            @unknown default:
+                self.errorUserCoordinateMessage()
+                break
+            }
+            
+        } else {
+            self.enableGPSMessage()
+        }
+    }
+    
+    /*
+        Responding to request location result
+     */
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.locationManager.stopUpdatingLocation()
+        self.setUserCoordinate(coordinate: locations[0].coordinate)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        self.errorUserCoordinateMessage()
+    }
+    
+    
+    /*
+        Presenting the results with the qeuried location.
+     */
+    func setUserCoordinate (coordinate: CLLocationCoordinate2D) {
+        self.coordinate = coordinate
+        self.getNextPage(latitude: coordinate.latitude, longitude: coordinate.longitude, searchString: "") {
+            self.tableView.reloadData()
+        }
+    }
+    
+    /*
+        Ask the user for activating the GPS
+     */
+    func enableGPSMessage () {
+        
+    }
+    
+    /*
+        Tell the user that there were ane error while getting his location.
+     */
+    func errorUserCoordinateMessage () {
         
     }
 }
---
+++ test tabela/models/LocaisModel.swift
@@ -0,0 +1,55 @@
+//
+//  Result.swift
+//  test tabela
+//
+//  Created by user155532 on 5/12/19.
+//  Copyright © 2019 sgelves. All rights reserved.
+//
+
+import UIKit
+
+protocol LocaisModel: NSObject {
+    
+    var resultData: [ Local] { get set }
+    var currentPage: Int { get set }
+    var isFetchingLoais: Bool { get set }
+    
+}
+
+extension LocaisModel {
+    
+    func getNextPage(latitude: Double, longitude: Double, searchString: String, completionHandler: @escaping () -> Void) {
+        guard (self.isFetchingLoais || self.currentPage < 0) else {
+            return
+        }
+        self.isFetchingLoais = true
+        ResultsService.queryResults(page: self.currentPage, latitude: latitude,
+                                    longitude: longitude, searchString: searchString) { data in
+            guard data != nil else {
+                self.isFetchingLoais = false
+                return
+            }
+            
+            for local in data! {
+                self.resultData.append(local)
+            }
+            if (data!.count < 15) {
+                self.currentPage = -1
+            } else {
+                self.currentPage += 1
+            }
+                                        
+            self.isFetchingLoais = false
+            completionHandler()
+        }
+        
+    }
+    
+    func restartState() {
+        guard !self.isFetchingLoais else {
+            return
+        }
+        self.resultData = []
+        self.currentPage = 0
+    }
+}
